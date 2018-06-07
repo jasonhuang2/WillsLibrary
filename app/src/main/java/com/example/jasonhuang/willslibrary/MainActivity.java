b84b539cd0b41dda3b592a3facf4e7486a1672ad
jasonhuang <verbatim238>
2018-06-06 19:03:47 -0600

    Main Menu: - Can now select the type of user that is logging in
    * Added new layout for the user's main menu called which can be accessable once the user successfully logs in.

--- app/src/main/java/com/example/jasonhuang/willslibrary/MainActivity.java
+++ app/src/main/java/com/example/jasonhuang/willslibrary/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.jasonhuang.willslibrary;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.sql.Connection;
@@ -15,11 +19,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Objects;
 
-import javax.xml.transform.Result;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     Connection conn;
     String un, pass, db, ip;
@@ -37,43 +38,27 @@ public class MainActivity extends AppCompatActivity {
         db = "DB_A3C994_will";    //emter database name here
         un = "DB_A3C994_will_admin";    //enter username here
         pass = "willslibrary1";  //enter password here
-    }
 
-    //Make sure to link the method to the button using the activity_main.xml file, click on the button, go to properties
-    //and add this method under "onClick" property
-    public void onButtonClickConnect(View v){
-        conn = connectionclass(un, pass, db, ip);
 
-        EditText t1 = (EditText)findViewById(R.id.text);
 
-        if(conn == null){
-            t1.setText("No");
-        }else{
-            t1.setText("Yes");
 
-            try {
-                //Inserting into the database with db = database name
-                //dbo.names is the table name
-                //standard format for accessing the sql server provided through Tamer is the following
-                String query = "INSERT INTO " + db + ".dbo.names VALUES (3,'Dudesdfasdfasdfasdf');";
-                Statement stmt = conn.createStatement();
-                stmt.execute(query);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
     }
 
 
-
+    /**
+     * This function is responsible once the "LOGIN" button is pressed.
+     *
+     */
     public void loginButtonClick(View v){
-        conn = connectionclass(un, pass, db, ip);
+        conn = connectionclass(un, pass, db, ip);   //I need this so I can query to the database
 
-        EditText usernameInputBox = (EditText)findViewById(R.id.usernameInputBox);
-        EditText passwordInputBox = (EditText)findViewById(R.id.passwordInputBox);
-        TextView testText = (TextView)findViewById(R.id.textView);
+        EditText usernameInputBox = (EditText)findViewById(R.id.usernameInputBox);  //The username input box, I have to find the ID of it
+        EditText passwordInputBox = (EditText)findViewById(R.id.passwordInputBox);  //The password input box, I have to find the ID of it
+        TextView resultText = (TextView) findViewById(R.id.loginTextView2);     //I want to print out the result of entering a username and password and after querying the database
+        Spinner userOrAdminSpinner = (Spinner) findViewById(R.id.user_or_admin_spinner);    //The spinner that is used to select what type of user is logging in
 
 
+        //The actual query string
         String query = "SELECT * FROM DB_A3C994_will.dbo.users WHERE username='" + usernameInputBox.getText().toString()+"';";
         try{
             Statement stmt = conn.createStatement();
@@ -83,42 +68,32 @@ public class MainActivity extends AppCompatActivity {
             if(rs.next()){
                 String result = rs.getString("password");
                 if(passwordInputBox.getText().toString().equals(result)){
-                    testText.setText("Logged in");
+
+                    if(userOrAdminSpinner.getSelectedItem().toString().equals("User")){
+                        resultText.setText("Logged in");    //Set the text to Logged in to let the user know
+                        Intent intent = new Intent(this, userMainActivity.class);   //Since now the user is logged in, switch over to the user main menu layout
+                        startActivity(intent);
+                    }else{
+                        resultText.setText("Sorry you are not an admin");
+                    }
+
                 }else{
-                    testText.setText("Incorrect username or password");
+                    resultText.setText("Incorrect username or password");
                 }
 
-
             }else{
-                testText.setText("Something went wrong with the query");
-
+                resultText.setText("Incorrect username or password");
             }
 
         }catch (SQLException e){
             e.printStackTrace();
-            testText.setText("EXPLODED");
+            resultText.setText("EXPLODED");
 
         }
-
 
 
     }
 
-    public void testButtonClick(View v){
-        TextView onoff = (TextView) findViewById(R.id.testTEST2);
-        Button onoffButton = (Button) findViewById(R.id.test_button);
-
-        if(onoff.getText().equals("OFF")){
-            onoff.setText("ON");
-            onoffButton.setText("ON");
-        }
-        else{
-            onoff.setText("OFF");
-            onoffButton.setText("OFF");
-        }
-
-
-    }
     //connection class
     @SuppressLint("NewApi")
     public Connection connectionclass (String user, String pass, String db, String server){
@@ -138,4 +113,6 @@ public class MainActivity extends AppCompatActivity {
 
         return connection;
     }
+
+
 }
---
+++ app/src/main/java/com/example/jasonhuang/willslibrary/userMainActivity.java
@@ -0,0 +1,99 @@
+package com.example.jasonhuang.willslibrary;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class userMainActivity extends AppCompatActivity {
+
+    Connection conn;
+    String un, pass, db, ip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        //Any layout that requires access to the database must have this
+        conn = connectionclass(un, pass, db, ip);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_main_activity);
+
+        //NOTE: Fill these attributes before you execute this program
+        //NOTE: For ip address, if you are provided with a port number, the format will be "ipaddress:portnumber"
+        ip = "sql5004.site4now.net:1433";    //enter ip address here
+        db = "DB_A3C994_will";    //emter database name here
+        un = "DB_A3C994_will_admin";    //enter username here
+        pass = "willslibrary1";  //enter password here
+
+        //This is for the back button but I dont think the user's main page needs a back button back to the login page
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        //https://stackoverflow.com/questions/4211189/how-to-get-id-in-different-layout
+        View inflatedView = getLayoutInflater().inflate(R.layout.main_activity, null);  //I did this so I can access the username from the main_activity layout
+        EditText text = (EditText)inflatedView.findViewById(R.id.passwordInputBox);    //Now everything is accessable through text, since text now points to the passwordInputBox from main_activity
+
+        TextView nameText = (TextView)findViewById(R.id.nameText);
+
+
+        /*
+        Don't delete I am trying to do something here, I wanted to print out the name. Will come back to it later.
+        String userName = text.getText().toString();
+        String nameQuery = "SELECT first_name FROM " + db + ".dbo.users WHERE username='" + userName + "' LIMIT 1;";
+
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(nameQuery);
+
+            if(rs.next()){
+
+                String result = rs.getString("first_name");
+
+                nameText.setText(result);
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+
+        }
+
+        */
+
+
+    }
+
+
+
+    //connection class
+    @SuppressLint("NewApi")
+    public Connection connectionclass (String user, String pass, String db, String server){
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String connectionURL = null;
+
+        try{
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            connectionURL = "jdbc:jtds:sqlserver://" + server + ";" + "databseName=" + db + ";user=" + user + ";password=" + pass + ";";
+            connection = DriverManager.getConnection(connectionURL);
+
+        }catch(Exception e){
+            Log.e("Error: ", e.getMessage());
+        }
+
+        return connection;
+    }
+}
