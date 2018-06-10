9adb31611b9b78a23aefb817f46068aef377304f
Shaheed M <shem.murji@gmail.com>
2018-06-10 11:38:44 -0600

    Added User Main Menu Buttons and initialized their activities

---
+++ app/src/main/java/com/example/jasonhuang/willslibrary/myrentalMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jasonhuang.willslibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class myrentalMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_myrental_main);
+    }
+}
