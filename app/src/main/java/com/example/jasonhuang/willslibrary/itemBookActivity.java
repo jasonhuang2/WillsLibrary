18831abc09610fc5ed471981cfb8bc88875bff53
Shaheed M <shem.murji@gmail.com>
2018-06-12 19:21:38 -0600

    Author works (except multiple authors)

--- app/src/main/java/com/example/jasonhuang/willslibrary/itemBookActivity.java
+++ app/src/main/java/com/example/jasonhuang/willslibrary/itemBookActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class itemBookActivity extends AppCompatActivity {
 
     @Override
@@ -14,18 +16,21 @@ public class itemBookActivity extends AppCompatActivity {
 
 
         String title_string = getIntent().getExtras().getString("title_string");
+        String author_string = getIntent().getExtras().getString("author_string");
         String genre_string = getIntent().getExtras().getString("genre_string");
         String publisher_string = getIntent().getExtras().getString("publisher_string");
         String publishing_date_string = getIntent().getExtras().getString("publishing_date_string");
         String description_string = getIntent().getExtras().getString("description_string");
 
         TextView titleNameBox = (TextView)findViewById(R.id.titleTextViewBox);
+        TextView authorBox = (TextView) findViewById(R.id.authorTextViewBox);
         TextView genreBox = (TextView)findViewById(R.id.genreTextViewBox);
         TextView publisherBox = (TextView)findViewById(R.id.publisherTextViewBox);
         TextView publishing_date_Box = (TextView)findViewById(R.id.publishingDateTextView);
         TextView descriptionBox = (TextView)findViewById(R.id.descriptionTextViewBox);
 
         titleNameBox.setText(title_string);
+        authorBox.setText(author_string);
         genreBox.setText(genre_string);
         publisherBox.setText(publisher_string);
         publishing_date_Box.setText(publishing_date_string);
--- app/src/main/java/com/example/jasonhuang/willslibrary/userMainActivity.java
+++ app/src/main/java/com/example/jasonhuang/willslibrary/userMainActivity.java
@@ -90,15 +90,30 @@ public class userMainActivity extends AppCompatActivity {
                             String publisher_string = bookrs.getString("publisher");
                             String publishing_date_string = bookrs.getString("publishing_date");
                             String description_string = bookrs.getString("description");
-                            //Passing the information to the activity to be displayed
-                            Intent itemBookPage = new Intent(this, itemBookActivity.class);
-                            itemBookPage.putExtra("title_string", title_string);
-                            itemBookPage.putExtra("genre_string", genre_string);
-                            itemBookPage.putExtra("publisher_string", publisher_string);
-                            itemBookPage.putExtra("publishing_date_string", publishing_date_string);
-                            itemBookPage.putExtra("description_string", description_string);
+                            //Obtaining the author information
+                            String authorquery = "SELECT * FROM DB_A3C994_will.dbo.author WHERE b_isbn='" + isbn + "';";
+                            try{
+                                Statement authorstmt = conn.createStatement();
+                                ResultSet authorrs = authorstmt.executeQuery(authorquery);
 
-                            startActivity(itemBookPage);
+                                if(authorrs.next()) {
+                                    String author_string = authorrs.getString("author_name");
+                                    //Passing the information to the activity to be displayed
+                                    Intent itemBookPage = new Intent(this, itemBookActivity.class);
+                                    itemBookPage.putExtra("title_string", title_string);
+                                    itemBookPage.putExtra("author_string", author_string);
+                                    itemBookPage.putExtra("genre_string", genre_string);
+                                    itemBookPage.putExtra("publisher_string", publisher_string);
+                                    itemBookPage.putExtra("publishing_date_string", publishing_date_string);
+                                    itemBookPage.putExtra("description_string", description_string);
+
+                                    startActivity(itemBookPage);
+                                } else {
+                                    Log.d("query", "Author does not exist in the database");
+                                }
+                            } catch (SQLException e) {
+                                Log.d("query", "Query Failed");
+                            }
                         }
                         else{
                             Log.d("query", "Book does not exist in the database");
